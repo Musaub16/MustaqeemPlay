be57ee26d1f0d14e83b263d40aa001482bcb2ce2
Darda <dardausa@gmail.com>
2021-03-30 21:31:25 -0500

    First commit

---
+++ src/com/mustaqeem/activities/MustaqeemWalk.java
@@ -0,0 +1,17 @@
+package com.mustaqeem.activities;
+
+public class MustaqeemWalk {
+	
+	public static void main(String[] arg) {
+		
+		String walk = "yes";
+		
+		if(walk == "yes") {
+			System.out.println("Mustaqeem is walk right now");
+		}else {
+			System.out.println("Mustaqeem only sleep...");
+		}
+		
+	}
+
+}
